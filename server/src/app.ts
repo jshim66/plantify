f2ad341d49877b7dcdd0024c5f32f70de92500da
yujoostudios <jshim66@gmail.com>
2026-03-12 20:58:42 -0700

    Router intro

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,15 @@
 import Express = require("express");
 const app = Express();
 
+app.set('view engine', 'ejs');
+
 app.get("/", (req, res) => {
     console.log("hi")
     res.send("sup")
 })
+
+const healthRoutes = require('./routes/health_routes');
+
+app.use('/api', healthRoutes);
 
 app.listen(4000);
